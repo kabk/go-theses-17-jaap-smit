1b0589ba852b9cdc7491a29c816cba73aeeb2215
Jaap Smit No.3 <Jaap@Jaaps-MacBook-Pro.local>
2017-01-27 14:21:59 +0100

    New

---
+++ js/swapImg.js
@@ -0,0 +1,8 @@
+function changeImage(element, element2) {
+	$("#imageReplace").attr("src",element);
+	$("#imageDescription").text(element2);
+}
+
+function changeText(element) {
+	$("#imageDescription2").text(element);
+}
